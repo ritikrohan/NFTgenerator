01560a3374d291469e39eefebee2dcebee94b44c
ritik <ritikdps@gmail.com>
2022-01-03 11:34:42 +0530

    Add neccesary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import GridLines from "react-gridlines";
+import { Rnd } from "react-rnd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const defaultProps = {
+  height: "400px",
+  width: "200px",
+  depth: 0,
+  x: 0,
+  y: 0,
+};
+
+export default class app extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: { ...defaultProps },
+      b: { ...defaultProps },
+    };
+  }
+  render() {
+    return (
+      <div
+        style={{
+          width: "8000px",
+          height: "4000px",
+        }}
+      >
+        <GridLines
+          className="grid-area"
+          cellWidth={60}
+          strokeWidth={2}
+          cellWidth2={12}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div
+            style={{
+              width: "800px",
+              height: "400px",
+            }}
+          >
+            <Rnd bounds="window" style={{ zIndex: 0 }}>
+              <img src={require("./layers/ball/red eye ball_sr.png")} alt="x" />
+            </Rnd>
+          </div>
+          <div
+            style={{
+              width: "800px",
+              height: "400px",
+            }}
+          >
+            <Rnd bounds="window" style={{ zIndex: 1 }}>
+              <img src={require("./layers/iris/large.png")} alt="x" />
+            </Rnd>
+          </div>
+        </GridLines>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/GridLines.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import GridLines from "react-gridlines";
+
+export default class gridlines extends Component {
+  render() {
+    return (
+    );
+  }
+}
