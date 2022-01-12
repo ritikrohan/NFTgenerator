5a46d231536b4a905d0a9799d5822acdb2b905bd
ritik <ritikdps@gmail.com>
2022-01-12 23:49:50 +0530

    Add styling to the sidebar

--- src/BuildFolder.js
+++ src/BuildFolder.js
@@ -7,28 +7,29 @@ import { useTheme } from "@material-ui/core/styles";
 import { TreeView } from "@material-ui/lab";
 import TreeItem from "@material-ui/lab/TreeItem";
 import { Typography } from "@material-ui/core";
+import "./buildFolder.css";
 
-const commonStyle = {
-  margin: "0px",
-  alignItems: "center",
-  lineHeight: 1.5,
-  letterSpacing: "0px",
-  fontFamily:
-    "IBM Plex Sans -apple-system BlinkMacSystemFont Segoe UI Roboto Helvetica Neue Arial sans-serif Apple Color Emoji Segoe UI Emoji Segoe UI Symbol",
-  fontWeight: 500,
-  display: "x",
-  borderRadius: "5px",
-  outline: "0px",
-  width: "100%",
-  paddingTop: "5px",
-  paddingBottom: "5px",
-  justifyContent: "flex-start",
-  transition:
-    "color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-  fontSize: "0.875rem",
-  color: "#b2bac2",
-  paddingLeft: "31px",
-};
+// const commonStyle = {
+//   margin: "0px",
+//   alignItems: "center",
+//   lineHeight: 1.5,
+//   letterSpacing: "0px",
+//   fontFamily:
+//     "IBM Plex Sans -apple-system BlinkMacSystemFont Segoe UI Roboto Helvetica Neue Arial sans-serif Apple Color Emoji Segoe UI Emoji Segoe UI Symbol",
+//   fontWeight: 500,
+//   display: "x",
+//   borderRadius: "5px",
+//   outline: "0px",
+//   width: "100%",
+//   paddingTop: "5px",
+//   paddingBottom: "5px",
+//   justifyContent: "flex-start",
+//   transition:
+//     "color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+//   fontSize: "0.875rem",
+//   color: "#b2bac2",
+//   paddingLeft: "31px",
+// };
 
 export const Folders = (props) => {
   const children = props.children;
@@ -46,12 +47,19 @@ export const Folders = (props) => {
         children.map((folder) => (
           <div>
             <ListItem button component="a" href="#">
-              <Typography style={commonStyle}>{folder.name}</Typography>
+              <Typography className="element">{folder.name}</Typography>
             </ListItem>
 
             {folder.children && <Folders children={folder.children} />}
           </div>
         ))}
+      <TreeItem
+        label={
+          <ListItem root component="a" href="#">
+            <ListItemText primary={" "} />
+          </ListItem>
+        }
+      />
     </div>
   );
 };
